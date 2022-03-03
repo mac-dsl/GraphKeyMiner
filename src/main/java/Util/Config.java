fc03bd9f4d0fa9b6105067ae50750973d232dc7d
Morteza <morteza.alipur@gmail.com>
2022-03-03 12:32:50 -0500

    Added new options to save summary graph in the Config file

--- src/main/java/Util/Config.java
+++ src/main/java/Util/Config.java
@@ -22,10 +22,10 @@ public class Config {
     public static boolean saveKeys=true;
     public static boolean debug =false;
     public static boolean preprocessOptimization = true;
-    public static boolean saveSummaryGraph = false;
+    public static boolean saveSummaryGraph = true;
     public static boolean saveSummaryGraphBasedOnDelta = true;
 
-    public static void parse(String input) throws FileNotFoundException {
+    public static boolean parse(String input) {
         if(input.equals("--help")) {
             System.out.println("""
                      Expected arguments to parse:
@@ -34,14 +34,22 @@ public class Config {
                      -k <integer> // Maximum size of the GKey
                      -delta <double> // Minimum coverage threshold of the GKey
                      -type <String> // Type of the center node
-                     -optgraphload <true-false> // load based on specific types?
+                     -optimizedLoadingBasedOnTypes <true-false> // load based on specific types?
                      -debug <true-false> // print details of matching
                      -save <true-false> // Save the mined GKeys
                      -language <language name> // Names like "N-Triples", "TURTLE", "RDF/XML"
                      -dataset <dataset name> // Options: imdb (default), dbpedia, synthetic
+                     -preprocessOptimization <true-false> // find uniqueness of attributes in the pre-process
+                     -saveSummaryGraph <true-false> // Save the summary graph in a separate file to be loaded later
+                     -saveSummaryGraphBasedOnDelta <true-false> // Save the summary graph based on delta in a separate file in a more readable format
                     """.indent(5));
-        } else
+            return false;
+        }
+        else
+        {
             parseInputParams(input);
+            return true;
+        }
     }
 
     private static void parseInputParams(String pathToConfigFile) {
@@ -53,18 +61,20 @@ public class Config {
                 String[] conf = line.toLowerCase().split(" ");
                 if (conf.length != 2)
                     continue;
-                switch (conf[0]) {
+                switch (conf[0].toLowerCase()) {
                     case "-t" -> typesPaths.add(conf[1]);
                     case "-d" -> dataPaths.add(conf[1]);
                     case "-k" -> k = Integer.parseInt(conf[1]);
                     case "-delta" -> delta = Double.parseDouble(conf[1]);
                     case "-type" -> type = conf[1];
-                    case "-optgraphload" -> optimizedLoadingBasedOnTypes = Boolean.parseBoolean(conf[1]);
+                    case "-optimizedloadingbasedontypes" -> optimizedLoadingBasedOnTypes = Boolean.parseBoolean(conf[1]);
                     case "-debug" -> debug = Boolean.parseBoolean(conf[1]);
                     case "-save" -> saveKeys = Boolean.parseBoolean(conf[1]);
                     case "-language" -> language = conf[1];
                     case "-dataset" -> dataset = conf[1];
-                    case "-preopt" -> preprocessOptimization = Boolean.parseBoolean(conf[1]);
+                    case "-preprocessoptimization" -> preprocessOptimization = Boolean.parseBoolean(conf[1]);
+                    case "-saveSummaryGraph" -> saveSummaryGraph = Boolean.parseBoolean(conf[1]);
+                    case "-saveSummaryGraphBasedOnDelta" -> saveSummaryGraphBasedOnDelta = Boolean.parseBoolean(conf[1]);
                 }
             }
         } catch (FileNotFoundException e) {
